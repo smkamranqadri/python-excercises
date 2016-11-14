bfc55a54e9735fbf6cdf7e7d48ad693a127d673a
Muhammad Kamran <muhammadkamran@Muhammads-MacBook-Pro.local>
2016-11-14 18:41:04 +0500

    solution to excercise 4

--- excercise-4.py
+++ excercise-4.py
@@ -5,3 +5,16 @@
 # Suppose the following input is supplied
 # to the program:hello world! 123Then,
 # the output should be:LETTERS 10DIGITS 3
+
+letter = 0
+digit = 0
+
+param = input("Please enter sequence to calculate Letters and Digists?")
+
+for i, c in enumerate(param):
+    if(str.isalpha(c)):
+        letter = letter + 1
+    if(str.isdigit(c)):
+        digit = digit + 1
+
+print('Letter: ' + str(letter) + ', Digit: ' + str(digit))
